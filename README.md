e0c6aef6fb21d5d96c8f8c06c74bf6bc844c7c4c
Aaron <aaronclayton94@hotmail.com>
2020-04-30 13:15:08 +0800

    passed all tests and functions correctly

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Template-Engine---Employee-Summary
+
+# Team Template Generator
+Build a software engineering team generator command line application. The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. This assignment must also pass all unit tests. When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user
+
+## Repository url / Deployed application url
+
+## Table of Contents
+- [Team Template Generator](#team-template-generator)
+  - [Repository url / Deployed application url](#repository-url--deployed-application-url)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+
+## Installation
+npm install npm i inquirer npm i jest
+
+## Usage
+jest, node, inquirer
+
+## Contributing
+
+
+## Tests
+npm run test // jest
+
--- app.js
+++ app.js
@@ -10,5 +10,93 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const teamMembers = []
+const teamMembers = [];
 
+async function askquestions() {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What the employee's name?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What the employee's id?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What the employee's email address?"
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "Select the employee's role?",
+        choices: ["Manager", "Engineer", "Intern"]
+      }
+    ]);
+  
+  
+    switch (answers.role) {
+      case "Manager":
+        const phone = await inquirer.prompt([
+          {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?"
+          }
+        ]);
+        teamMembers.push(
+          new Manager(answers.name, answers.id, answers.email, phone.officeNumber)
+        );
+        addAnotherEmployee();
+        break;
+      case "Engineer":
+        const gitHub = await inquirer.prompt([
+          {
+            type: "input",
+            name: "gitHubUsername",
+            message: "What is the engineer's github username?"
+          }
+        ]);
+        teamMembers.push(
+          new Engineer(answers.name, answers.id, answers.email, gitHub.gitHubUsername)
+        );
+        addAnotherEmployee();
+        break;
+      case "Intern":
+        const school = await inquirer.prompt([
+          {
+            type: "input",
+            name: "school",
+            message: "What is your interns school?"
+          }
+        ]);
+        teamMembers.push(
+          new Intern(answers.name, answers.id, answers.email, school.school)
+        );
+        addAnotherEmployee();
+        break;
+      default:
+    }
+}
+askquestions();
+  
+async function addAnotherEmployee() {
+  const addMoreEmployee = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "addAgain",
+      message: "Do you want to add another employee?"
+    }
+  ]);
+    
+    addMoreEmployee.addAgain == true ? askquestions() : buildTeam(teamMembers);
+  }
+  function buildTeam(teamMembers) {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
